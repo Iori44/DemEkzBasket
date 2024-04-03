1c51006d540352dff446b8ca424a71de0bb675fe
Iori44 <133634347+Iori44@users.noreply.github.com>
2024-04-03 14:27:57 +0500

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = tbLogin.Text;
+            string password = tbPassword.Password;
+
+            // Ваш код проверки логина и пароля
+        }
+
+        private void LoginWindow_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
